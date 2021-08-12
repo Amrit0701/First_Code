e4224ea6762b2a7b6cb11336c3decb29b4b792a9
Amrit Anand <a0a0b7o@homeoffice.wal-mart.com>
2021-08-12 20:01:50 +0530

    Create_3_file_Delete_Print_Directory

---
+++ myfifthscript.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+touch amrit1 amrit2 amrit3
+echo In the amrit1 file > amrit1
+echo In the amrit2 file > amrit2
+echo In the amrit3 file > amrit3
+cat amrit1
+cat amrit2
+cat amrit3
+rm amrit1 amrit2 amrit3
+pwd
+
